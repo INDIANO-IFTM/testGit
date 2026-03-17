137d1a34ae06bf149739f9f10139ba9578af97f6
Indiano <henrique.aj@estudante.iftm.edu.br>
2026-03-17 08:07:01 -0300

    Foi desenvolvido o programa de fatorial

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-n = (input('Digite um número: '))
+def fatorial(n: int) -> int:
+    '''
+        algoritmo iterativ para resolver fatorial
+        input:
+            n:int - Um valor inteiro qualquer>8
+        output:
+            result - Um valor inteiro >0
+    '''
+    res = 1
+    for i in range(1, n+1):
+        res *= i #= a res = res * i
+    return res
 
 try:
-    n = int(n)
-    print(n)
-
+    print('===== Fatorial =====')
+    n = int(input('Digite um numero'))
+    print(fatorial(n))
 except ValueError:
-    print("Erro")
-
+    print('Erro! Voce deve entrar com um numero')
